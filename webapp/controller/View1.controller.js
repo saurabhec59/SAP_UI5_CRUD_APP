517ef9578618c9da86be31af03fe956165223c6d
saurabhec59 <saurabhyadav2690@gmail.com>
2026-06-22 18:51:52 +0530

    Update operation - to increase the age of emp by 1 on clicking on list i

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -26,6 +26,7 @@ sap.ui.define([
             this.getView().setModel(model);
         },
 
+        // Read
         onEmpListItemPress: function(oEvent){
             var data = oEvent.getSource().getBindingContext().getObject();
             // getSource() will return the object of control which fired this event, here it is list item.
@@ -35,6 +36,7 @@ sap.ui.define([
             MessageToast.show("Name: " + data.name + "Age: " + data.age);
         },
 
+        // Delete
         // modifying the press function of list item to delete the list item after clicking on it.
         onEmpListItemPress2: function(oEvent){
             var itemIndex = oEvent.getSource().getBindingContext().getPath().split("/").pop();
@@ -54,6 +56,23 @@ sap.ui.define([
             this.getView().getModel().setProperty("/employees", empArray);
         },
 
+        // update
+        // Requirement is to increate the age of emp by 1 on clicking on list i
+        onEmpListItemPress3: function(oEvent){
+            // getting path till age of clicked emp
+            var path = oEvent.getSource().getBindingContext().getPath() + "/age";
+            // getting age value
+            var age = this.getView().getModel().getProperty(path);
+            // updating age value by 1
+            this.getView().getModel().setProperty(path, age + 1);
+            // using getProperty() we can get age value of clicked employee
+            // using setProperty() we can set the new value of age to model
+            // but both needs tha path till the "age" property of clicked emp and that
+            // using oEvent.getSource().getBindingContext().getPath()+"/age" we can get path till age of clicked emp.
+            // using getView().getModel().getProperty() or setProperty() we can fetch or set the value.
+        },
+
+        // Create
         onAddEmpPress: function(){
             var name = this.getView().byId("empNameInput").getValue();
             var age = this.getView().byId("empAgeInput").getValue();
