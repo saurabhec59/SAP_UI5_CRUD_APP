428ac37fe4f4da37d4a35499dc908c536ec00fff
saurabhec59 <saurabhyadav2690@gmail.com>
2026-07-01 21:02:49 +0530

    make View2 display the emp details, create and assign local model to View2 and update related control bindings

--- webapp/controller/View2.controller.js
+++ webapp/controller/View2.controller.js
@@ -16,11 +16,30 @@ sap.ui.define([
         },
 
         onRouteMatched: function(oEvent){
-            console.log(oEvent.getParameter("arguments").id);
+            // fetching id parameter passed in route url
+            var searchId = oEvent.getParameter("arguments").id;
+            // fetching the employees array from empModel stored at component level
+            var empData = this.getOwnerComponent().getModel("empModel").getProperty("/employees");
+            var i = 0;
+            // iterating over the employees array to find the employee object matching id passed in route url
+            while(i < empData.length){
+                if(empData[i].id === Number(searchId)){
+                    // creating and assigning a local named model to view2 containing only one array element with matched id
+                    var localView2Model = new JSONModel(empData[i]);
+                    this.getView().setModel(localView2Model, "localView2Model");
+                    break;
+                }
+                i++;
+            }
+            
             // whenever a route is matched, that route object itself fires an event called 'patternMatched' which containes the info about paremeters passed in the route.
             // attachPatternMatched() job is to register a callback function like 'onRouteMatched' that should be called whenever the 'patternMatched' event is fired.
             // getParameter() => when patternMatched event contains the info about the parameters passed in the route via navTo() and getParameter("arguments") will return the object of all the parameters passed in the route. 
             MessageToast.show("Route matched with id: " + oEvent.getParameter("arguments").id);
+        },
+
+        onNavigateToView1: function(){
+            this.getOwnerComponent().getRouter().navTo("RouteView1");
         }
     })
 })
